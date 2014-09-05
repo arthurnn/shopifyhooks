65210e305ca5e27c10f935e2fb89d93bae0383c7
Arthur Nogueira Neves <arthurnn@gmail.com>
2014-09-05 16:21:29 -0400

    Bump revision

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-hook_version=0.0.3
+hook_version=0.0.4
 if [ -d "/home/vagrant/src/shopify/.git/hooks" ]; then
     cd /home/vagrant/src/shopify/.git/hooks
     wget -qO- "https://github.com/arthurnn/shopifyhooks/archive/v$hook_version.tar.gz" | tar xzv --strip-components=1 --index-file=shopifyhooks.log
