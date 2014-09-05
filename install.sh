64603e26bc53cc7c4246c1da3cb87f204fe07de6
Arthur Neves <arthurnn@gmail.com>
2014-09-05 15:50:44 -0400

    Fix download command

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
+#!/bin/sh
 
+hook_version=v0.0.1
 if [ -d "/home/vagrant/src/shopify/.git/hooks" ]; then
     cd /home/vagrant/src/shopify/.git/hooks
-    git clone https://github.com/arthurnn/shopifyhooks.git .
+    wget -qO- "https://github.com/arthurnn/shopifyhooks/archive/$hook_version.tar.gz" | tar xz --strip-components=1
     rm -f README.md install.sh
+    echo "Done."
+    exit 0
 else
     echo "You are not inside vagrant, please vagrant-up!"
     exit 1
